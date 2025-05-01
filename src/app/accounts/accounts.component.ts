86a62f3103e9000e9b88819b26cc1aa3de146627
hamza <hamza.benchekchou@usmba.ac.ma>
2025-05-01 19:54:30 +0100

    add ..

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-accounts',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
-export class AccountsComponent {
+export class AccountsComponent  implements OnInit{
+  accountFormGroup!:FormGroup
+
+constructor(private fb:FormBuilder){
+
+}
+
+  ngOnInit(): void {
+    this.accountFormGroup=this.fb.group({
+
+    });
+  }
+
+
 
 }
