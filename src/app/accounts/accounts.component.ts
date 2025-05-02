99fa4db88db2b79c12b49523cd6b358a3226d832
hamza <hamza.benchekchou@usmba.ac.ma>
2025-05-02 15:15:15 +0100

    add Account

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent  implements OnInit{
+
   accountFormGroup!:FormGroup
 
 constructor(private fb:FormBuilder){
@@ -16,9 +17,14 @@ constructor(private fb:FormBuilder){
 
   ngOnInit(): void {
     this.accountFormGroup=this.fb.group({
+      accountId:this.fb.control('')
 
     });
   }
+
+  handleSearchAccount() {
+    throw new Error('Method not implemented.');
+    }
 
 
 
